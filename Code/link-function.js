a0f8a43a2113f6a356192b23bc39830cc7f78fce
hemsaicharanyarlagadda <hemsaicharan_yarlagadda@srmap.edu.in>
2026-07-20 00:09:31 +0530

    Delete Code/link-function.js

--- Code/link-function.js
+++
@@ -1,8 +0,0 @@
-/* Sports Zone - Link Function */
-function link(scope, element, attrs, controller) {
-    // Custom link function logic
-    // This runs after the widget is rendered
-    
-    // Example: Add custom animations or third-party integrations
-    console.log('Sports Zone widget loaded');
-}
